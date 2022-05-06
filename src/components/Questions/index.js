6b667b8e5cb4a32022058803f8fd82643c34405d
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-05-06 21:26:51 +0200

    bouton de validation passe de désactivé à activé lors de la sélection de l'option de réponse et activation d'une class pour le <p> de l'option cliquée

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Questions = (props) => {
+    const [validate, setValidate] = useState(false);
+    const [userAnswer, setUserAnswer] = useState(null);
 
-  const displayOptions = props.options.map((option, index) => {
-      return(
-        <p key={index} className="answerOptions">{option}</p>
-      )
-  })
+    const optionValidation = (selectedOption) => {
+        setUserAnswer(selectedOption);
+        setValidate(true);
+    };
 
-  return (
-    <>
-    <h2>{props.question}</h2>
-    { displayOptions }
-    <button className="btnSubmit">Suivant</button>
-    </>
-  )
-}
+    const displayOptions = props.options.map((option, index) => {
+        return (
+            <p
+                key={index}
+                className={`answerOptions ${userAnswer === option ? 'selected' : null}`}
+                onClick={() => optionValidation(option)}
+            >
+                {option}
+            </p>
+        );
+    });
+
+    const buttonValidation =
+        validate === true ? (
+            <button className='btnSubmit'>Suivant</button>
+        ) : (
+            <button className='btnSubmit' disabled>
+                Suivant
+            </button>
+        );
+
+    return (
+        <>
+            <h2>{props.question}</h2>
+            {displayOptions}
+            {buttonValidation}
+        </>
+    );
+};
 
 export default Questions;
